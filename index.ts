7eddf201eca6c352a00c51e045f84a0dd9124f7d
Cheryl Lee <clee0937@gmail.com>
2025-03-24 14:49:34 +0000

    updated type annotation for priceBracket from string to number

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const deliveryTimeMax = "90";
 const maxDistance = 10;
 let result;
 
-const priceBracket: string = dollarSigns.length;
+const priceBracket: number = dollarSigns.length;
 
 const filteredRestaurants = restaurants.filter((restaurant) => {
   if (restaurant.priceBracket > priceBracket) {
